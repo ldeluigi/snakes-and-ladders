f1e5afafc54d9463db5251c69c7e6b1abf90b651
Andrea Cardiota <andrea.cardiota@studio.unibo.it>
2020-09-19 12:32:57 +0200

    HotFix dice priority and implement snake

--- src/main/scala/CustomValues.scala
+++ src/main/scala/CustomValues.scala
@@ -4,12 +4,6 @@ import untitled.goose.framework.model.events.persistent.TurnEndedEvent
 
 trait CustomValues {
   val gooseGroup = "GooseTile"
-  val firstSnakeHead = "Snake One Head"
-  val firstSnakeTailEnd = "Snake One Tail End"
-  val secondSnakeHead = "Snake Two Head"
-  val secondSnakeTailEnd = "Snake Two Tail End"
-  val thirdSnakeHead = "Snake Three Head"
-  val thirdSnakeTailEnd = "Snake Three Tail End"
   val bottomLadder = "Bottom Ladder"
   val topLadder = "Top Ladder"
   val snakeHead = "Snake Head"
--- src/main/scala/SnakeAndLadders.scala
+++ src/main/scala/SnakeAndLadders.scala
@@ -16,28 +16,33 @@ object SnakeAndLadders extends GooseDSL with CustomValues {
   The game board has size(30)
   the game board has disposition(Snake)
 
-  The tiles(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29) have color(Red)
+  The tiles(1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29) have color(Orange)
   The tiles(2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30) have color(White)
 
-  The tile 1 has name(firstSnakeTailEnd)
-  The tile 4 has name(secondSnakeTailEnd)
-  The tile 7 has name(thirdSnakeTailEnd)
-  The tile 27 has name(firstSnakeHead)
-  The tile 17 has name(secondSnakeHead)
-  The tile 19 has name(thirdSnakeHead)
+  The tiles(7, 12, 13, 18, 19, 24) have background("ladder.png")
+  The tile 27 has background("snakeOne.png")
+  The tile 28 has background("snakeTwo.png")
+  The tile 22 has background("snakeThree.png")
+  The tile 21 has background("snakeFour.png")
+  The tile 20 has background("snakeFourB.png")
+  The tile 15 has background("snakeFive.png")
+  The tile 14 has background("snakeFiveB.png")
+  The tile 16 has background("snakeSix.png")
+  The tile 10 has background("snakeSeven.png")
+  The tile 9 has background("snakeEight.png")
+  The tile 8 has background("snakeEightB.png")
+  The tile 3 has background("snakeNine.png")
+  The tile 2 has background("snakeTen.png")
 
-  The tiles(11, 14, 23) have background("ladder.png")
-  The tiles(4, 9, 16) have background("ladder.png")
-  The tiles(18, 19) have background("ladder.png")
 
   The tile 30 has(
     name(theEnd),
-    //background("victory.png"),
+    background("victory.png"),
     color(Gold)
   )
 
-  val snakeMap: Map[Int, Int] = Map(17 -> 1, 19 -> 4, 27 -> 7)
-  val ladderMap: Map[Int, Int] = Map(4 -> 16, 11 -> 25, 18 -> 19)
+  val snakeMap: Map[Int, Int] = Map(28 -> 2)
+  val ladderMap: Map[Int, Int] = Map(12 -> 24, 7 -> 19)
 
   The tiles (snakeMap.keys.toList: _*) have group(snakeHead)
   The tiles (snakeMap.values.toList: _*) have group(snakeTail)
@@ -50,7 +55,7 @@ object SnakeAndLadders extends GooseDSL with CustomValues {
 
   Players loseTurn priority is 10
 
-  Each turn players are (always allowed to roll 1 movementDice "six-faced" as "roll a dice" priority 5)
+  Each turn players are (always allowed to roll 1 movementDice "six-faced" as "roll a dice" priority 1)
 
   //To win you must reach tile 30 exactly.
   // If your dice roll is more than you need then you move in to tile 30 bounce back out again,
